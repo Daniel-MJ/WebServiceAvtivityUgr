9d05e08d9ef526c08a27c2bd4b8ba142533bd959
Dani <danimrjm96@gmail.com>
2023-10-31 20:54:19 +0100

    Commit inicial

---
+++ src/ExampleRestlet.java
@@ -0,0 +1,24 @@
+
+import org.restlet.Component;
+import org.restlet.data.Protocol;
+import org.restlet.resource.ClientResource;
+
+public class ExampleRestlet {
+
+	public static void main(String[] args) throws Exception {  
+	    // Create a new Component.  
+	    Component component = new Component();  
+
+	    // Add a new HTTP server listening on port 8182.  
+	    component.getServers().add(Protocol.HTTP, 8182);  
+
+	    // Attach the sample application.  
+	    component.getDefaultHost().attach("/firstSteps",  
+	            new FirstStepsApplication());  
+
+	    // Start the component.  
+	    component.start();  
+		//new ClientResource("http://restlet.com").get().write(System.out);
+	}
+
+}
