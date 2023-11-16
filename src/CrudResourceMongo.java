76f672cc6964b2d6179e2db37df1af53ebdd8916
Dani <danimrjm96@gmail.com>
2023-11-16 13:30:01 +0100

    cv

--- src/CrudResourceMongo.java
+++ src/CrudResourceMongo.java
@@ -1,10 +1,23 @@
 import org.bson.Document;
-import org.restlet.resource.*;
+import org.restlet.representation.JsonRepresentation;
+import org.restlet.resource.Get;
+import org.restlet.resource.Post;
+import org.restlet.resource.Delete;
+import org.restlet.resource.Put;
+import org.restlet.resource.ServerResource;
+import org.restlet.data.Status;
+import org.restlet.resource.PathVariable;
 import org.restlet.representation.Representation;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 import java.util.List;
 
 public class CrudResourceMongo extends ServerResource {
+
+    // Crear la conexión a MongoDB en la inicialización
     private MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
     private MongoDatabase database = mongoClient.getDatabase("mi_base_de_datos");
     private MongoCollection<Document> collection = database.getCollection("mi_coleccion");
@@ -31,10 +44,7 @@ public class CrudResourceMongo extends ServerResource {
     @Post("json")
     public void handlePost(Representation entity) {
         // Método para crear un nuevo documento
-        Document newDocument = new Document();
-        // Parsear el JSON de la entidad y agregar campos al nuevo documento
-        // ...
-
+        Document newDocument = Document.parse(entity.getText());
         // Insertar el nuevo documento en la colección
         collection.insertOne(newDocument);
     }
@@ -42,10 +52,7 @@ public class CrudResourceMongo extends ServerResource {
     @Put("json/{id}")
     public void handlePut(@PathVariable("id") String id, Representation entity) {
         // Método para actualizar un documento existente por ID
-        Document updatedDocument = new Document();
-        // Parsear el JSON de la entidad y actualizar campos en el documento
-        // ...
-
+        Document updatedDocument = Document.parse(entity.getText());
         // Realizar la actualización en la colección
         collection.replaceOne(new Document("_id", id), updatedDocument);
     }
