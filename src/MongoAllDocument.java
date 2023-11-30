7a63c66b9f08b307f2a4ffd15f879067b114ca42
Dani <danimrjm96@gmail.com>
2023-11-30 13:40:13 +0100

    cv

--- src/MongoAllDocument.java
+++ src/MongoAllDocument.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
 import java.util.List;
-
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 import org.bson.Document;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Resource which has only one representation.
@@ -10,7 +11,7 @@ import org.bson.Document;
 public class MongoAllDocument extends ServerResource {
 
     @Get("json")
-    public List<Document> getAllDocuments() {
+    public List<String> getAllDocumentsAsJson() {
         // Obtener el parámetro "miBasedeDatos" de la solicitud
         String databaseName = getQueryValue("miBasedeDatos");
         
@@ -21,6 +22,17 @@ public class MongoAllDocument extends ServerResource {
 
         // Obtener todos los documentos usando MongoDBConnector
         MongoDBManager connector = new MongoDBManager(databaseName, collectionName);
-        return connector.getAllDocuments();
+        List<Document> allDocuments = connector.getAllDocuments();
+
+        // Convierte los documentos BSON a JSON
+        List<String> allDocumentsJson = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        for (Document document : allDocuments) {
+            String json = document.toJson();
+            allDocumentsJson.add(json);
+        }
+
+        return allDocumentsJson;
     }
 }
